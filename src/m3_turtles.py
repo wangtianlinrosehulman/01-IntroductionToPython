465ee8fe1696b620aba413254ac8e5b880bcaacc
wangt2 <wangt2@rose-hulman.edu>
2018-03-05 10:54:45 -0500

    DONE

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Tianlin Wang.
 """
 ###############################################################################
 #
-# TODO: 1.
+# Done: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
 #   On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2.
+# Done: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -49,7 +49,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 3.
+# Done: 3.
 #   Run this module.  A window will pop up and Turtles will move around.
 #   After the Turtles stop moving, *click anywhere in the window to close it*.
 #
@@ -105,7 +105,7 @@ natasha.forward(50)
 
 ###############################################################################
 #
-# TODO: 4.
+# done: 4.
 #   Add a few more line of your own code to make one of the
 #   existing SimpleTurtles move some more and/or have different
 #   characteristics.
@@ -116,10 +116,14 @@ natasha.forward(50)
 #   As always, test by running the module.
 #
 ###############################################################################
-
+natasha.speed = 2
+natasha.right(45)
+natasha.forward(20)
+natasha.right(45)
+natasha.forward(20)
 ###############################################################################
 #
-# TODO: 5.
+# Done: 5.
 #   The above code  CONSTRUCTS  two SimpleTurtle objects
 #   and gives those objects NAMES:
 #       boris    natasha
@@ -143,10 +147,13 @@ natasha.forward(50)
 #   As always, test by running the module.
 #
 ###############################################################################
-
+mark = rg.SimpleTurtle('turtle')
+mark.forward(20)
+mark.right(60)
+mark.forward(30)
 ###############################################################################
 #
-# TODO: 6.
+# Done: 6.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
